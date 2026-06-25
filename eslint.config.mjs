8dee2e75dbe81e6debf3920ccc0a77ecfb4201c1
fafromweak <abdulwahidltd@gmail.com>
2026-06-25 11:43:51 +0500

    fix: revert eslint config to clean version without storybook plugin reference

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,3 @@
-// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
-import storybook from "eslint-plugin-storybook";
-
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
@@ -25,7 +22,6 @@ const eslintConfig = defineConfig([
       "react-hooks/purity": "off",
     },
   },
-  ...storybook.configs["flat/recommended"]
 ]);
 
 export default eslintConfig;
